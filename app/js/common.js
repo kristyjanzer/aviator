73015f3b0c5933d2bb0082bd65513d2bca6975eb
Kristina Yancer <kristyjanzer.work@gmail.com>
2025-03-08 16:24:41 +0300

    fix

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,8 @@
+// Open menu
 $(function() {
-  $(function() {
-    $('.header-menu-more').click(function() {
-      $(this).toggleClass('active');
-    });
-});
+  $('.header-menu-more').click(function() {
+    $(this).toggleClass('active');
+  });
 });
 
 // Burger Menu
@@ -19,16 +18,47 @@ burgerMenu.addEventListener("click", (e) => {
   body.classList.toggle("lock");
   e.stopPropagation();
 
+
   if($('.header-menu-more').hasClass('active')) {
     $(".header").addClass("header-menu-open");
     $(".header .header-button-menu__body").addClass("active");
   }
+
+  // $(".header-menu-more").click(function() {
+  //   $(".header").removeClass("header-menu-open");
+  //   $(".header .header-button-menu__body").removeClass("active");
+  // });
 });
 
 $(".header-menu").clone().appendTo(".header-button-menu__body");
 
-$(".header-menu-more").addEventListener("click", (e) => {
-  $(".header").removeClass("header-menu-open");
-  $(".header .header-button-menu__body").removeClass("active");
-  e.stopPropagation();
-});
+
+
+// Accordion
+let AccordionBlock = function (el, multiple) {
+  this.el = el || {};
+  // more then one submenu open?
+  this.multiple = multiple || false;
+
+  let accordionMenuLink = this.el.find('.table-of-contents__text');
+  accordionMenuLink.on('click',
+    { el: this.el, multiple: this.multiple },
+    this.dropdown);
+};
+
+AccordionBlock.prototype.dropdown = function (e) {
+  let $el = e.data.el,
+    $this = $(this),
+    //this is the ul.table-of-contents__content
+    $next = $this.next();
+
+  $next.slideToggle();
+  $this.parent().toggleClass('open');
+
+  if (!e.data.multiple) {
+    //show only one menu at the same time
+    $el.find('.table-of-contents__content').not($next).slideUp().parent().removeClass('open');
+  }
+}
+
+let accordionBlock = new AccordionBlock($('.table-of-contents'), false);
