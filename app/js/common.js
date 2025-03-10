682b8324a36e8bc3898d1c2ec2f8a8f917e21b6c
Kristina Yancer <kristyjanzer.work@gmail.com>
2025-03-10 17:28:56 +0300

    fix

--- app/js/common.js
+++ app/js/common.js
@@ -127,3 +127,33 @@ $(document).on("click", ".m-tabs-menu__item", function() {
 		$(".m-tabs-info").height(listItemHeight + "px");
 	}
 });
+
+
+
+// ----------------- Variables
+
+wrapper   = $(".tabs");
+tabs      = wrapper.find(".tab");
+tabToggle = wrapper.find(".tab-toggle");
+
+// ----------------- Functions
+
+function openTab() {
+	var content     = $(this).parent().next(".content"),
+		activeItems = wrapper.find(".active");
+	
+	if(!$(this).hasClass('active')) {
+		$(this).add(content).add(activeItems).toggleClass('active');
+		wrapper.css('min-height', content.outerHeight());
+	}
+};
+
+// ----------------- Interactions
+
+tabToggle.on('click', openTab);
+
+// ----------------- Constructor functions
+
+$(window).load(function(){
+  tabToggle.first().trigger('click');  
+});
